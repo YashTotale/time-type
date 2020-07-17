1364c6ea0ae4a53fb88ebc3b57d1ced3e33ac6fe
YashTotale <totaleyash@gmail.com>
2020-07-17 10:43:47 -0700

    refactoring done; added selectors for getLoading and getError

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -4,3 +4,5 @@ export const getIsSidebarOpen = (state) => state.isSidebarOpen;
 export const getWordList = (state) => state.typer.wordList;
 export const getCurrentWord = (state) => state.typer.currentWord;
 export const getDifficulty = (state) => state.typer.difficulty;
+export const getIsTyperLoading = (state) => state.typer.isLoading;
+export const getIsTyperError = (state) => state.typer.isError;
