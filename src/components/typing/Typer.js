e3a3aa238e24693b3f662f93f9c878fcbf885fb5
YashTotale <totaleyash@gmail.com>
2020-07-17 11:18:20 -0700

    added skeletons for TyperTabs and TyperInput

--- src/components/typing/Typer.js
+++ src/components/typing/Typer.js
@@ -5,13 +5,20 @@ import TyperInput from "./TyperInput";
 
 // Redux Imports
 import { useDispatch, useSelector } from "react-redux";
-import { getWordList, getCurrentWord, getDifficulty } from "../../selectors";
+import {
+  getWordList,
+  getCurrentWord,
+  getDifficulty,
+  getIsTyperLoading,
+  getIsTyperError,
+} from "../../selectors";
 import {} from "../../actions";
 import { getWordsRequest } from "../../thunks";
 
 // Material UI Imports
 import { makeStyles } from "@material-ui/core/styles";
 import { Paper, Tabs, Tab } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import {} from "@material-ui/icons";
 import TyperTabs from "./TyperTabs";
 
@@ -22,8 +29,13 @@ const useTyperStyles = makeStyles((theme) => ({
     minWidth: "50%",
     maxWidth: "960px",
   },
-  typingPaper: {
+  typerPaper: {
     marginTop: "64px",
+  },
+  typerTabs: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
   words: {
     margin: "0px 10px 10px",
@@ -42,13 +54,23 @@ const Typer = ({}) => {
   //Dispatch
   const dispatch = useDispatch();
   //Variables
+  const isLoading = useSelector(getIsTyperLoading);
+  const isError = useSelector(getIsTyperError);
   const wordList = useSelector(getWordList);
   const currentWord = useSelector(getCurrentWord);
   return (
     <>
       <div className={classes.typer} id="typer">
-        <Paper className={classes.typingPaper} variant="outlined" elevation={3}>
-          <TyperTabs />
+        <Paper className={classes.typerPaper} variant="outlined" elevation={3}>
+          <div className={classes.typerTabs}>
+            {isLoading ? (
+              <Skeleton variant="rect">
+                <TyperTabs />
+              </Skeleton>
+            ) : (
+              <TyperTabs />
+            )}
+          </div>
           <div className={classes.words}>
             {wordList.map((word, i) => {
               return (
@@ -60,7 +82,13 @@ const Typer = ({}) => {
               );
             })}
           </div>
-          <TyperInput currentWord={currentWord} />
+          {isLoading ? (
+            <Skeleton width="100%" variant="rect">
+              <TyperInput />
+            </Skeleton>
+          ) : (
+            <TyperInput currentWord={currentWord} />
+          )}
         </Paper>
       </div>
     </>
